442ea410e909b04288d0ea8b6cf61dc73dd27ebc
NagarjunaPosa <95393454+arjunposa@users.noreply.github.com>
2023-12-07 19:22:57 +0530

    fourthCommit

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const connectToDB = require('./MongoDB/DBConnection')
-const userRouter = require('./Routers/UserRouter')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const connectToDB = require("./MongoDB/DBConnection");
+const userRouter = require("./Routers/UserRouter");
+const cors = require("cors");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({}))
+app.use(cors({}));
 
-
+app.get("/", (req, res) => {
+  res.send("Api is Working ");
+});
 //user Router
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 
-                 app.listen(5000, async ()=>{
-    await connectToDB()
-    console.log("server is running ")
-})
+app.listen(5000, async () => {
+  await connectToDB();
+  console.log("server is running ");
+});
